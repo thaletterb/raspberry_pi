2707de81c3024657340aeccc82441a4df9705311
Brian <thaletterb@gmail.com>
2017-01-07 23:19:51 -0800

    add barebones setup_device()

--- kernel_dev/tsl2561/src/tsl2561_driver.c
+++ kernel_dev/tsl2561/src/tsl2561_driver.c
@@ -18,6 +18,13 @@ MODULE_AUTHOR("Brian Vuong");
 MODULE_DESCRIPTION("Controls a TSL2561 Lux Sensor");
 MODULE_VERSION("0.1");
 
+typedef struct sensor_data_t
+{
+	struct input_dev *device;
+}sensor_data_t;
+
+sensor_data_t *sensor_data;
+
 /*
 *	I2C Setup
 */
@@ -25,6 +32,41 @@ MODULE_VERSION("0.1");
 /*
 *	Device Setup
 */
+
+static int setup_device(void)
+{
+	int result = 0;
+
+	// allocate memory to store our pad data
+	sensor_data = kzalloc(sizeof(sensor_data_t), GFP_KERNEL);
+
+	// create the device
+	sensor_data->device = input_allocate_device();
+	if(!sensor_data->device)
+	{
+		printk(KERN_ERR "Not enough memory\n");
+		return -1;
+	}
+
+	// setup device description
+	sensor_data->device->name = "TSL2561";
+	sensor_data->device->phys = "TSL2561_phys";
+	sensor_data->device->uniq = "TSL2561_uniq";
+
+	sensor_data->device->evbit[0] = BIT_MASK(EV_ABS);
+
+	// setup the sensor
+	// setup_device_axis(pad_data->device, ABS_X);
+	// setup_device_axis(pad_data->device, ABS_Y);
+
+	result = input_register_device(sensor_data->device);
+	if(result)
+	{
+		printk(KERN_ERR "Failed to register tsl2561 device. Result: %d\n", result);
+	}
+
+	return result;
+}
 
 /*	
 *	Driver Lifecycle
@@ -34,6 +76,14 @@ int tsl2561_init(void)
 	s32 value;
 	int result = 0;
 
+	result = setup_device();
+	if(result)
+	{
+		printk(KERN_INFO "FAIL: Could not setup tsl2561 device. Result: %d\n", result);
+		// input_free_device(pad_data->device)
+		goto finish;
+	}
+
 	printk(KERN_INFO "loading tsl2561 driver v 0.1\n");
 
 finish:
@@ -42,7 +92,7 @@ finish:
 
 void tsl2561_exit(void)
 {
-
+	printk(KERN_INFO "unloading tsl2561 driver v 0.1\n");
 }
 
 module_init(tsl2561_init);
